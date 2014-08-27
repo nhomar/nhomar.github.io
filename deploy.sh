d39a36d4f617b8c2b20069be252d9c85f5625138
Nhomar - Vauxoo <nhomar@gmail.com>
2014-08-27 16:17:49 -0430

    [FIX] The downloading of the theme must be before install not in deploy time

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 BRANCH=master #Usage of master is mandatory for username and organization pages, try gh-pages for project pages.
 TARGET_REPO=nhomar/nhomar.github.io.git
 PELICAN_OUTPUT_FOLDER=output
-PELICAN_BOOTSTRAP_BRANCH="master"
-PELICAN_BOOTSTRAP_REPO="nhomar/pelican-bootstrap3.git"
 
 echo -e "Testing travis-encrypt"
 echo -e "$VARNAME"
@@ -15,7 +13,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.name "Travis"
     fi
     #using token clone gh-pages, or master branch
-    git clone --quiet --branch=$PELICAN_BOOTSTRAP_BRANCH https://github.com/$PELICAN_BOOTSTRAP_REPO $HOME/pelican-bootstrap3 > /dev/null
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
