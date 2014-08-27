c3eadefe8e57031acb7b79dc15dbc26e2dd11cba
Nhomar - Vauxoo <nhomar@gmail.com>
2014-08-27 16:07:24 -0430

    [FIX] asking pelican-bootstrap3 theme in Home, it is a more generic way

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS" == "true" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
-        git clone --quiet --branch=$PELICAN_BOOTSTRAP_BRANCH https://github.com/$PELICAN_BOOTSTRAP_REPO $HOME/pelican-bootstrap3 > /dev/null
     fi
     #using token clone gh-pages, or master branch
+    git clone --quiet --branch=$PELICAN_BOOTSTRAP_BRANCH https://github.com/$PELICAN_BOOTSTRAP_REPO $HOME/pelican-bootstrap3 > /dev/null
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
